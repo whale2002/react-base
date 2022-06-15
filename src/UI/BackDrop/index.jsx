4b07dda25cb244ecc3bbda55e47a26ad4153d049
chin <1277180540@qq.com>
2022-06-15 09:37:59 +0800

    feat: add portal

--- src/UI/BackDrop/index.jsx
+++ src/UI/BackDrop/index.jsx
@@ -1,11 +1,13 @@
-import './index.css';
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const backDropRoot = document.getElementById("backdrop");
 
 const BackDrop = (props) => {
-    return (
-        <div className="backDrop">
-            {props.children}
-        </div>
-    );
+  return ReactDOM.createPortal(
+    <div className="backDrop">{props.children}</div>,
+    backDropRoot
+  );
 };
 
 export default BackDrop;
